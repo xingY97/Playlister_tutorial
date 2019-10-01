266368eb79053069668872a5ca9419dc119b7722
Xing_Ye <YOUR_PERSONAL_EMAIL@example.com>
2019-10-01 11:04:15 -0700

    user sees the list

--- app.py
+++ app.py
@@ -2,19 +2,23 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-@app.route('/')
-def index():
-    playlists = [
+# @app.route('/')
+# def index():
+#     """Return homepage."""
+#     return render_template('home.html', msg='Flask is Cool!')
+
+    #Our mock array of project
+playlists = [
     { 'title': 'Cat Videos', 'description': 'Cats acting weird' },
     { 'title': '80\'s Music', 'description': 'Don\'t stop believing!' }
 ]
-@app.route('/playlists')
+
+@app.route('/')
 def playlists_index():
     """Show all playlists."""
     return render_template('playlists_index.html', playlists=playlists)
     
-    """Return homepage."""
-    return render_template('home.html', msg='Flask is Cool!')
+
 
 if __name__ == '__main__':
     app.run(debug=True)
