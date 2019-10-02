9917b7e5b4312e6479f6bdb2246bb5fd5e93a36e
Xing_Ye <YOUR_PERSONAL_EMAIL@example.com>
2019-10-01 18:00:36 -0700

    card added

--- app.py
+++ app.py
@@ -68,7 +68,5 @@ def playlists_delete(playlist_id):
     return redirect(url_for('playlists_index'))
 
     
-
-
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=os.environ.get('PORT', 5000))
