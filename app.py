a1fd77d57c71632ad12083fbb475676df4586114
Xing_Ye <YOUR_PERSONAL_EMAIL@example.com>
2019-10-01 17:14:07 -0700

    heroku

--- app.py
+++ app.py
@@ -3,9 +3,9 @@ from pymongo import MongoClient
 from bson.objectid import ObjectId
 import os
 
-# client = MongoClient(host=f'{host}?retryWrites=false')
+
 host = os.environ.get('MONGODB_URI', 'mongodb://localhost:27017/Playlister')
-client = MongoClient(host=host)
+client = MongoClient(host=f'{host}?retryWrites=false')
 db = client.get_default_database()
 playlists = db.playlists
 
